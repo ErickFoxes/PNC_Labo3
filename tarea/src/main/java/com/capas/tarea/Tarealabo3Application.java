c0351a8b3822fbbf73589adcfc2472ac6d658c07
ErickFoxes <37157947+ErickFoxes@users.noreply.github.com>
2020-05-06 02:14:08 -0300

    Inicio

---
+++ tarea/src/main/java/com/capas/tarea/Tarealabo3Application.java
@@ -0,0 +1,13 @@
+package com.capas.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarealabo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarealabo3Application.class, args);
+	}
+
+}
---
+++ tarea/src/test/java/com/capas/tarea/Tarealabo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capas.tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarealabo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
